b4b58fcbd35ae1f3b3ba562ae6935e00071a7dd0
Lucas Martins Lima <lucas.martins.lima@avanade.com>
2022-08-16 19:57:22 -0300

    fix: implementação de navegação entre telas no módulo de main

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '../../material.module';
+import { MaterialModule } from './../../material.module';
 import { AuthComponent } from './components/auth.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDzXOK72MEXvQcnUNFQGXuPxRpTLSZOSQc",
+    authDomain: "wefeed-7a48f.firebaseapp.com",
+    projectId: "wefeed-7a48f",
+    storageBucket: "wefeed-7a48f.appspot.com",
+    messagingSenderId: "885820963601",
+    appId: "1:885820963601:web:26fb5c00cd70af9a76aab0",
+    measurementId: "G-LR6L9KMYCZ"
+  }
 };
 
 /*
