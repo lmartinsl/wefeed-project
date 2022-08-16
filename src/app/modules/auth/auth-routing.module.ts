d76a36ad27deabdd154f3cfa1d5c058672f97f8c
Lucas Martins Lima <lucas.martins.lima@avanade.com>
2022-08-15 22:36:15 -0300

    fix: Ajustes e criação de components + install lib pwa

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,3 +1,4 @@
+import { ResetComponent } from './components/reset/reset.component';
 import { AuthComponent } from './components/auth.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'auth' },
   { path: 'auth', component: AuthComponent },
   { path: 'auth/login', component: LoginComponent },
-  { path: 'auth/register', component: RegisterComponent }
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'auth/reset', component: ResetComponent },
 ];
 
 @NgModule({
