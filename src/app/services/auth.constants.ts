dee9409a64f6d0313dc058b49d3dcd4451bc6b08
Edu Bernardo <eduluizbernardo@gmail.com>
2022-10-31 01:15:35 -0300

    feat: inclusao dos textos do modulo sobre nos

--- src/app/services/auth.constants.ts
+++ src/app/services/auth.constants.ts
@@ -1,6 +1,6 @@
 export class AuthApi {
-    static readonly BASE_URL= 'http://we-feed-api.sa-east-1.elasticbeanstalk.com';
-    static readonly LOGIN= 'http://we-feed-api.sa-east-1.elasticbeanstalk.com/auth';
+    static readonly BASE_URL= 'https://we-feed-api.sa-east-1.elasticbeanstalk.com';
+    static readonly LOGIN= 'https://we-feed-api.sa-east-1.elasticbeanstalk.com/auth';
     static readonly ACCEPTED= 'application/json';
 
 }
