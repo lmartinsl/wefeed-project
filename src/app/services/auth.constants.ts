79aee50db65f382ff29777f2082bbef2f3b43498
Edu Bernardo <eduluizbernardo@gmail.com>
2022-11-04 19:22:00 -0300

    fix: ajustando endpoint reset e validate

--- src/app/services/auth.constants.ts
+++ src/app/services/auth.constants.ts
@@ -3,8 +3,8 @@ export class AuthApi {
     static readonly BASE_URL= 'https://api.wefeed.click';
     static readonly LOGIN= 'https://api.wefeed.click/auth';
     static readonly REGISTER= 'https://api.wefeed.click/user/create';
-    static readonly VALIDATE= 'https://api.wefeed.click/validate';
-    static readonly RESET= 'https://api.wefeed.click/reset';
+    static readonly VALIDATE= 'https://api.wefeed.click/user/validate';
+    static readonly RESET= 'https://api.wefeed.click/user/reset';
 
 
 }
